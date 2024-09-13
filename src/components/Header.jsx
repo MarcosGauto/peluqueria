74cca754bc7c412854724f96c0a41c12ff55bccc
Marcos <mgauto@outlook.es>
2024-09-13 00:32:44 -0300

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,10 @@ function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clea
             <div className="container-xl">
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
-                        <a href="index.html">
-                            <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
-                        </a>
+
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <ul className="d-flex gap-4 list-unstyled mb-5 m-8">
+                    <ul className="d-flex gap-4 list-unstyled mb-5 m-8 w-100 mt-3 p-2 text-lg">
                             <li>
                                 <a className="text-white text-decoration-none" href="/">Home</a>
                             </li>
